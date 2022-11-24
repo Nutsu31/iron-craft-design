7432cc5fff51ad476100982cc5bda049e63be9fb
Nutsu31 <116025262+Nutsu31@users.noreply.github.com>
2022-11-25 03:27:39 +0400

    index html got responsive

--- js/index.js
+++ js/index.js
@@ -1,21 +1,10 @@
-const register = document.querySelector('.registration');
-const closerRegister = document.querySelector('.closerRegister')
-const registerLink = document.querySelector('.register')
+const bar = document.getElementById("bar");
+const headerMiddle = document.querySelector(".header-middle");
+const closer = document.getElementById("closer");
 
-registerLink.addEventListener('click', () =>  {
-    register.style.display = "flex";
+bar.addEventListener('click',() =>{
+    headerMiddle.style.display = "flex";
 });
-closerRegister.addEventListener("click", ()=>{
-    register.style.display = "none";
-})
-
-const loginer = document.querySelector(".loginer");
-const login = document.querySelector(".login");
-const closerLogin = document.querySelector(".closerLogin")
-
-loginer.addEventListener("click", () => {
-    login.style.display = "flex";
-})
-closerLogin.addEventListener("click", ()=>{
-    login.style.display = "none";
+closer.addEventListener('click',() => {
+    headerMiddle.style.display = "none";
 })
